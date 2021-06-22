6fbb57d9fdedc3de968de4fd89619750eef3c4c8
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-06-22 11:23:59 -0400

    create avatar component

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+
+import {
+  View,
+  Image
+} from 'react-native';
+
+import { styles } from './styles'
+import { theme } from '../../globals/styles/theme';
+
+type Props = {
+  urlImage: string;
+}
+
+export function Avatar({ urlImage }: Props) {
+  const { secondary50, secondary70 } = theme.colors;
+
+  return (
+    <LinearGradient
+      style={styles.container}
+      colors={[secondary50, secondary70]}
+    >
+      <Image
+      source={{ uri: urlImage }}
+      style={styles.avatar}
+      />
+    </LinearGradient>
+  )
+}
