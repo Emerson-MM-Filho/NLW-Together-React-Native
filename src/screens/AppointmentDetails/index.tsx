a4290a248187e12425000ce4b96f1d2b33e26547
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-06-23 18:42:26 -0400

    create appointment details component

---
+++ src/screens/AppointmentDetails/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Background } from '../../components/Background'
+
+export function AppointmentDetails() {
+  return (
+    <Background>
+      
+    </Background>
+  )
+}
