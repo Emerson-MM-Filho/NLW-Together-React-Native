bf09b2659ae087c303bea3852bf5f50bd655a8cf
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-06-23 18:05:40 -0400

    import new components, create fake appointments, finish flatlist

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,27 +1,55 @@
 import React, { useState } from 'react';
 import { View, FlatList, Text } from 'react-native';
+import { Appointment } from '../../components/Appointment';
 import { ButtonAdd } from '../../components/ButtonAdd';
 import { CategorySelect } from '../../components/CategorySelect';
 import { ListHeader } from '../../components/ListHeader';
 import { Profile } from '../../components/Profile';
+import { ListDivider } from '../../components/ListDivider';
 
 import { styles } from './styles'
 
 export function Home() {
   const [category, setCategory] = useState('');
 
-  const appointments = [{
-    id: '1',
-    guild: {
+  const appointments = [
+    {
       id: '1',
-      name: 'Lendários',
-      icon: null,
-      owner: true,
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true,
+      },
+      category: '1',
+      date: '22/06 às 20:40h',
+      description: 'É hoje que vamos chegar ao challender sem perder uma partida da md10'
     },
-    category: '1',
-    date: '22/06 às 20:40h',
-    description: 'É hoje que vamos chegar ao challender sem perder uma partida da md10'
-  }]
+    {
+      id: '2',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true,
+      },
+      category: '1',
+      date: '22/06 às 20:40h',
+      description: 'É hoje que vamos chegar ao challender sem perder uma partida da md10'
+    },
+    {
+      id: '3',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true,
+      },
+      category: '1',
+      date: '22/06 às 20:40h',
+      description: 'É hoje que vamos chegar ao challender sem perder uma partida da md10'
+    }
+  ]
 
   function handleCategorySelect(categoryId: string) {
     categoryId === category ? setCategory('') : setCategory(categoryId);
@@ -29,33 +57,30 @@ export function Home() {
 
   return (
     <View>
-
       <View style={styles.header}>
         <Profile />
         <ButtonAdd />
       </View>
 
-      <View>
-        <CategorySelect
-          categorySelected={category}
-          setCategory={handleCategorySelect}
-        />
-        <View
-          style={styles.content}
-        >
-          <ListHeader title="Partidas agendadas" subtitle="Total 6"/>
+      <CategorySelect
+        categorySelected={category}
+        setCategory={handleCategorySelect}
+      />
 
-          <FlatList 
+      <View style={styles.content} >
+        <ListHeader title="Partidas agendadas" subtitle="Total 6"/>
+
+        <FlatList 
           data={appointments}
           keyExtractor={item => item.id}
           renderItem={({ item }) => (
-            <Text>{item.guild.name}</Text>
-          )}          
-          />
-
-        </View>
+            <Appointment data={item}/>
+          )}
+          ItemSeparatorComponent = {() => <ListDivider />}
+          style={styles.matches}
+          showsVerticalScrollIndicator={false}  
+        />
       </View>
-
     </View>
   )
 }
