a712e4dc04be43688b5a40f90cd813bde5b8beaf
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-06-24 14:49:03 -0400

    fix font family

--- src/components/ButtonIcon/styles.ts
+++ src/components/ButtonIcon/styles.ts
@@ -14,6 +14,7 @@ export const styles = StyleSheet.create({
     flex: 1,
     color: theme.colors.heading,
     fontSize: 15,
+    fontFamily: theme.fonts.text500,
     textAlign: 'center',
   },
   iconWrapper: {
