999005e748045ebbe079c8d686cc71a21d444762
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-06-24 11:08:31 -0400

    create appointment details component

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,12 +1,91 @@
-import React from 'react';
-import { View } from 'react-native';
+import React from 'react'
+import { Fontisto } from '@expo/vector-icons'
+import { BorderlessButton } from 'react-native-gesture-handler'
+import { 
+  ImageBackground,
+  Text,
+  View,
+  FlatList
+} from 'react-native'
 
+import { styles } from './styles'
+import { Header } from '../../components/Header'
+import { Member } from '../../components/Member'
 import { Background } from '../../components/Background'
+import { ListHeader } from '../../components/ListHeader'
+import { ButtonIcon } from '../../components/ButtonIcon'
+import { ListDivider } from '../../components/ListDivider'
+
+import { theme } from '../../globals/styles/theme'
+import bannerImg from '../../assets/banner.png'
 
 export function AppointmentDetails() {
+  const members = [
+    {
+      id: '1',
+      userName: 'Rodrigo',
+      avatarUrl: 'https://github.com/emerson-mm-filho.png',
+      status: 'online'
+    },
+    {
+      id: '2',
+      userName: 'Rodrigo',
+      avatarUrl: 'https://github.com/emerson-mm-filho.png',
+      status: 'offline'
+    }
+  ]
+
   return (
     <Background>
-      
+      <Header 
+        title='Detalhes'
+        action={
+          <BorderlessButton>
+            <Fontisto
+              name='share'
+              size={24}
+              color={theme.colors.primary}
+            />
+          </BorderlessButton>
+        }
+      />
+
+      <ImageBackground 
+        source={bannerImg}
+        style={styles.banner}
+      >
+        <View style={styles.bannerContent}>
+          <Text style={styles.title}>
+            Lendários
+          </Text>
+
+          <Text style={styles.subTitle}>
+            É hoje que vamos chegar ao challender sem perder uma partida da md10
+          </Text>
+        </View>
+      </ImageBackground>
+
+      <ListHeader 
+        title='Jogadores'
+        subtitle='Total 3'
+      />
+
+      <FlatList 
+        data={members}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <Member data={item}/>
+        )}
+        ItemSeparatorComponent={() => <ListDivider />}
+        style={styles.members}
+      />
+
+      <View style={styles.footer}>
+        <ButtonIcon
+          title='Entrar na partida'
+        />
+      </View>
+
     </Background>
   )
 }
