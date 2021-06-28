e09e6abbc507c29af5b10972aef068c55ed35bc1
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-06-28 10:41:30 -0400

    change width and height and remove margin

--- src/components/GuildIcon/styles.ts
+++ src/components/GuildIcon/styles.ts
@@ -3,9 +3,8 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   image: {
-    width:64,
-    height: 64,
+    width:62,
+    height: 62,
     borderRadius: 8,
-    marginRight: 20,
   },
 })
