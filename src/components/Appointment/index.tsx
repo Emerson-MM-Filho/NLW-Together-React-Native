848be7e1195ca45b784b6248f8a74e50f0e78565
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-06-28 10:40:05 -0400

    add linear gradient end change guildprops to import guildprops

--- src/components/Appointment/index.tsx
+++ src/components/Appointment/index.tsx
@@ -8,13 +8,8 @@ import CalendarSvg from '../../assets/calendar.svg'
 import { GuildIcon } from '../GuildIcon'
 import { categories } from '../../utils/categories';
 import { theme } from '../../globals/styles/theme';
-
-export type GuildProps = {
-  id: string,
-  name: string,
-  icon: null,
-  owner: boolean,
-}
+import { GuildProps } from '../Guild';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export type AppointmentProps = {
   id: string,
@@ -31,12 +26,17 @@ type Props = RectButtonProps & {
 export function Appointment({ data, ...rest }: Props) {
   const [category] = categories.filter(item => item.id === data.category)
   const { owner } = data.guild
-  const { primary, on } = theme.colors
+  const { primary, on, secondary50, secondary70 } = theme.colors
 
   return (
     <RectButton {...rest} >
       <View style={styles.container} >
-        <GuildIcon />
+        <LinearGradient
+          style={styles.guildIconConteiner}
+          colors={[ secondary50, secondary70 ]}
+        >
+          <GuildIcon />
+        </LinearGradient>
 
         <View style={styles.content} >
           <View style={styles.header} >
