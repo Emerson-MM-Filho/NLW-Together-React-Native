a374cfb0f177162353a17ea3713953efbef1917b
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-06-30 10:16:45 -0400

    change image and name to api response

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -3,14 +3,16 @@ import {
   View,
   Text,
 } from 'react-native';
+import { useAuth } from '../../hooks/auth';
 import { Avatar } from '../Avatar';
 
 import { styles } from './styles'
 
 export function Profile() {
+  const { user } = useAuth()
   return (
     <View style={styles.container}>
-      <Avatar urlImage='https://github.com/Emerson-MM-Filho.png'/>
+      <Avatar urlImage={user.avatar}/>
 
       <View>
         <View style={styles.user}>
@@ -20,7 +22,7 @@ export function Profile() {
           </Text>
 
           <Text style={styles.userName}>
-            Emerson
+            {user.firstName}
           </Text>
 
         </View>
