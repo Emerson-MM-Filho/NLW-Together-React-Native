03decb318eddf0bc1535460a4843989cd7f8f3f8
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-07-07 14:12:32 -0400

    add default guild icon and update to recive the icon from api

--- src/components/GuildIcon/index.tsx
+++ src/components/GuildIcon/index.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, View } from 'react-native';
 
 import { styles } from './styles'
+import DiscordSvg from '../../assets/discord.svg' 
 
-export function GuildIcon() {
-  const uri = 'https://github.com/emerson-mm-filho.png'
+const { CDN_IMAGE } = process.env
+
+type Props = {
+  guildId: string;
+  iconId: string | null;
+}
+
+export function GuildIcon({ guildId, iconId }: Props) {
+  const uri = `${CDN_IMAGE}/icons/${guildId}/${iconId}.png`
 
   return (
-    <Image 
-      source={{ uri }}
-      style={styles.image }
-      resizeMode='cover'
-    />
+    <View style={styles.container}>
+      {
+        iconId ? 
+        <Image 
+          source={{ uri }}
+          style={styles.image }
+          resizeMode='cover'
+        />
+        :
+        <DiscordSvg 
+          width={40}
+          height={40}
+        />
+      }
+    </View>
   )
 }
