1aaf6f474adc4349bb89a1637ac37f74fd9bfd42
Emerson-MM-Filho <emersonmmfilho@gmail.com>
2021-07-07 14:13:11 -0400

    update style to recive the guild icon from api

--- src/components/GuildIcon/styles.ts
+++ src/components/GuildIcon/styles.ts
@@ -1,10 +1,18 @@
 import { StyleSheet } from "react-native";
-
+import { theme } from "../../globals/styles/theme";
 
 export const styles = StyleSheet.create({
-  image: {
+  container: {
     width:62,
     height: 62,
     borderRadius: 8,
+    backgroundColor: theme.colors.discord,
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
   },
 })
